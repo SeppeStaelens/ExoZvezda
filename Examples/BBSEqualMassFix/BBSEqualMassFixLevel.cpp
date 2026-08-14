c7bef6e44072c2d3e90d7dbdbd6bc46398e0a985
GarethAMarks <gam54@cam.ac.uk>
2026-08-14 13:12:40 +0100

    Fix trailing space

--- Examples/BBSEqualMassFix/BBSEqualMassFixLevel.cpp
+++ Examples/BBSEqualMassFix/BBSEqualMassFixLevel.cpp
@@ -23,7 +23,7 @@
 #include "ADMQuantitiesExtraction.hpp"
 
 // For tag cells
-#include "BosonChiPunctureExtractionTaggingCriterion.hpp "
+#include "BosonChiPunctureExtractionTaggingCriterion.hpp"
 
 // Problem specific includes
 #include "BinaryEqualMassFix.hpp"
